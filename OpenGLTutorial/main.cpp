3c6c797c87bdd2e9f9dbb8bef1245ea17da2230d
ProjectHexx15 <amieam.mcgrory@gmail.com>
2026-07-05 19:38:13 +0100

    Created an Initial GLFW Window

--- OpenGLTutorial/main.cpp
+++ OpenGLTutorial/main.cpp
@@ -1,7 +1,87 @@
 #include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+// Callback function to adjust the viewport when the window is resized
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+
+// Function to process input from the user
+void processInput(GLFWwindow* window);
 
 int main() 
 {
-	std::cout << "Hello, OpenGL!" << std::endl;
+	// Initialize GLFW
+	glfwInit();
+
+	// Tells GLFW what version of OpenGL we are using (3.3)
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+	// Tells GLFW we are using the core profile, only modern functions are available
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	// This is needed to ensure that the code works on MacOS
+# ifdef __APPLE__
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+# endif
+
+	// Create a window
+	GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL Tutorial", NULL, NULL);
+
+	// Check if the window was created successfully, if not, print an error message and terminate GLFW
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	// set the current context to the window we just created
+	glfwMakeContextCurrent(window);
+
+	// Load GLAD so it configures OpenGL, if it fails, print an error message and terminate GLFW
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) 
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	// Specify the viewport of OpenGL in the window
+	glViewport(0, 0, 800, 600);
+
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	// Main render loop
+	while (!glfwWindowShouldClose(window))
+	{
+		// Input
+		processInput(window);
+
+		// render
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// send new frame to the screen
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
 	return 0;
 }
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window)
+{
+	// Check if the escape key is pressed, if so, set the window to close
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, true);
+	}
+}
+
